39328588897388b9d682898fd206609155718262
Astro Aguilar <theastrocodingproject@gmail.com>
2024-03-02 22:54:49 -0700

    solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = string => {
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+   return processedString.split("").reverse().join("") == processedString; 
 };
 
 // Do not edit below this line
